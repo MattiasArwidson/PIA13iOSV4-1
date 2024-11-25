4b95239b210deb835eea6ada6af9742c29dc97fd
Mattias Arwidson <mattias@actionawareness.se>
2024-11-25 09:19:06 +0100

    Initial Commit

---
+++ PIA13iOSV4-1/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  PIA13iOSV4-1
+//
+//  Created by Mattias Arwidson on 2024-11-25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ PIA13iOSV4-1/PIA13iOSV4_1App.swift
@@ -0,0 +1,17 @@
+//
+//  PIA13iOSV4_1App.swift
+//  PIA13iOSV4-1
+//
+//  Created by Mattias Arwidson on 2024-11-25.
+//
+
+import SwiftUI
+
+@main
+struct PIA13iOSV4_1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
